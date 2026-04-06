bdd3c544b9682755d5223104cb57d86321e3dceb
Matheus Henrique <mhenrique182005@gmail.com>
2026-04-06 19:26:24 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+//Plinio Henrique, Matheus Henrique, Marcos Roberto
+
+void insertionSort(int v[], int n) {
+    int i = 0;
+
+    while (i < n) {
+
+       
+        if (i == n - 1) {
+            int chave = v[i];
+            int j = i - 1;
+
+            while (j >= 0 && v[j] > chave) {
+                v[j + 1] = v[j];
+                j--;
+            }
+
+            v[j + 1] = chave;
+            break;
+        }
+
+       
+        int a = v[i];
+        int b = v[i + 1];
+        cout<<"Par analisado: " << a << " e " << b << endl;
+
+       
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+       
+        int j = i - 1;
+        while (j >= 0 && v[j] > b) {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = b;
+
+       
+        j--;
+        while (j >= 0 && v[j] > a) {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = a;
+
+       
+        i += 2;
+    }
+}
+
+int main() {
+    int v[] = {7, 2, 5, 4, 9, 1};
+    int n = sizeof(v) / sizeof(v[0]);
+    
+    cout << "Vetor original: ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    insertionSort(v, n);
+
+    cout << "Vetor ordenado: ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
